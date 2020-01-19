3e8154233363fc14f3f507bc6a18858f27b39975
twk <twk981114@gmail.com>
2020-01-19 15:34:20 +0800

    view

--- graph/views.py
+++ graph/views.py
@@ -255,12 +255,12 @@ def refresh(request):
     expected_daily_returns = [0.0007936507936507937, 0.001507936507936508, 0.0005555555555555556, 0.0011904761904761904, 0.0003492063492063492, 0.0004365079365079365, 0.0010317460317460319, 0.0004761904761904762, 0.00029365079365079364, 0.0011111111111111111]
     expected_annual_returns =  [0.2, 0.38, 0.14, 0.3, 0.088, 0.11, 0.26, 0.12, 0.074, 0.28]
     '''
-num_stocks = 3;
-stocks = ['AAPL', 'AMZN', 'CSCO']
-investment_duration = 1
-return_target = 0.22
-expected_daily_returns = [0.0007936507936507937, 0.001507936507936508, 0.0005555555555555556]
-expected_annual_returns =  [0.2, 0.38, 0.14]
+    num_stocks = 3;
+    stocks = ['AAPL', 'AMZN', 'CSCO']
+    investment_duration = 1
+    return_target = 0.22
+    expected_daily_returns = [0.0007936507936507937, 0.001507936507936508, 0.0005555555555555556]
+    expected_annual_returns =  [0.2, 0.38, 0.14]
 
     quandl.ApiConfig.api_key = 'PrW6L55BexiSBEqWU25u'
     data = quandl.get_table('WIKI/PRICES', 
